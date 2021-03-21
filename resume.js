3e0c84dd21c18498f061f63b4959ac90b5f1710e
Rayvon Finney Pinkston <rayfinn55@gmail.com>
2021-03-20 20:19:15 -0400

    2nd commit

--- resume.js
+++ resume.js
@@ -13,3 +13,11 @@ function scrollFunction() {
     document.body.scrollTop = 0; 
     document.documentElement.scrollTop = 0;
   }
+  function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
